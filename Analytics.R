73e047271ef359c631508f37885e49c2d75b6ea1
inyabuto <isaiahnyabuto@gmail.com>
2018-12-19 19:49:39 +0300

    Function documention and update on set up section

--- Analytics.R
+++ Analytics.R
@@ -1,18 +1,34 @@
 library(httr)
 library(jsonlite)
-#Login into the system 
+library(keyringr)
+
+
+#'
+#'Trigger Analytics resource tables
+#'
+
+#Set up ==========================================
+dest <- "192.168.0.198"
 dest.url <- "http://192.168.0.198/dhis/"
-dest.user <- "admin"
-dest.pass <- ""
+dest.user <- keyringr::get_kc_account(dest, type = "generic")
+dest.pass <- keyringr::decrypt_kc_pw(dest, type = "generic")
 
 
-
+#'
+#'Function to login into the API
+#'@param base.url, 
+#'@param usr
+#'@param pwd
+#'@return boolean
+#'
 loginDhis2 <- function(base.url, usr, pwd){
   url <- paste0(base.url, "api/me")
   r <- GET(url, authenticate(usr,pwd))
   assertthat::assert_that(r$status_code == 200)
 }
-#login
+
+#login and set time
+
 startTime <- Sys.time()
 loginDhis2(dest.url, dest.user, dest.pass)
 
@@ -21,6 +37,7 @@ loginDhis2(dest.url, dest.user, dest.pass)
 url <- paste0(dest.url,"api/resourceTables/analytics")
 r <- POST(url)
 
+# monitor the analytics
 completed <- FALSE
 while(completed == FALSE){
   Sys.sleep(10)
